a18ce0ce45f3240d910c653f01a7c30e5f0d2265
shadowcracks <yst187business@gmail.com>
2024-10-16 11:21:06 -0400

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 //import AdventureGallery from "@/components/AdventureGallery/AdventureGallery";
 import MainComponent from "@/components/MainComponent";
-import Image from "next/image";
-
 
 export default function Home() {
   return (
